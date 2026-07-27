87fd6dad8097d3fa90c8fab651d9001c15885444
Vildan Bina <vildanbina@gmail.com>
2026-07-27 14:11:43 +0200

    chore: shorten the app name to Buckaroo Official
    The Apps Store vendor guidelines ask for no more than 25 characters;
    "Payment Provider: Buckaroo Official" was 35. The shortened name also
    matches the provider record in payment_provider_data.xml, which was
    already "Buckaroo Official".

--- payment_buckaroo_official/__manifest__.py
+++ payment_buckaroo_official/__manifest__.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    "name": "Payment Provider: Buckaroo Official",
+    "name": "Buckaroo Official",
     "version": "19.0.1.0.0",
     "category": "Accounting/Payment Providers",
     "sequence": 350,
